83a25ec799b895a1878616c2d189ee69ce0630c5
Willem Boshoff <willem.boshoff@dyna-mo.com>
2022-06-23 15:12:25 +0200

    Update nvim-cmp keybindings

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -61,14 +61,16 @@ cmp.setup {
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-y>"] = cmp.mapping.confirm { select = true }, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-e>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.config.disable,
+    ["<Tab>"] = cmp.config.disable,
+    --[[ ["<CR>"] = cmp.mapping.confirm { select = true },
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -97,6 +99,7 @@ cmp.setup {
       "i",
       "s",
     }),
+  }, ]]
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
