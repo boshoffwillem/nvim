eef4260cea9f505583644aa6d1f8b6a53ed8049f
Willem Boshoff <boshoffwillem@protonmail.com>
2022-07-04 15:34:04 +0200

    configure yamlls for azure pipelines

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,14 +1,15 @@
 return {
     settings = {
         yaml = {
+            format = true,
+            schemaDownload = {
+                enable = true
+            },
             schemas = {
-                ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
-                ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
-                ["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
-                ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
-                ["https://azuremlschemas.azureedge.net/latest/pipelineJob.schema.json"] = "**/azure-pipelines.yml",
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*"
-            }
+                ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = { "/*Pipeline*.yml" },
+                ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml"
+            },
+            validate = true
         }
     }
 }
